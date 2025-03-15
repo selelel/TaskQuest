0df4e3fa131bc541601a3a20dcb60b34eb2ac5fa
selelel <janrusselgorembalem4@gmail.com>
2025-03-15 20:28:13 +0800

    init: linting

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,9 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 
+import reactX from 'eslint-plugin-react-x'
+import reactDom from 'eslint-plugin-react-dom'
+
 export default tseslint.config(
   { ignores: ['dist'] },
   {
@@ -16,6 +19,8 @@ export default tseslint.config(
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react-x': reactX,
+      'react-dom': reactDom,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
